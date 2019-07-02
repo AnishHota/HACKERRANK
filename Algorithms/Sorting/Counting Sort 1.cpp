b20bd10083caf10904dadff84adac31421e39f3a
anishhota <anishhota92@gmail.com>
2019-07-02 23:31:51 +0530

    Adds a solution to Algorithms

---
+++ Algorithms/Sorting/Counting Sort 1.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+  long long t,temp;
+  cin>>t;
+  long long arr[t] = {0};
+  for(long long i=0;i<t;i++)
+  {
+    cin>>temp;
+    arr[temp]++;
+  }
+  for(auto x:arr)
+    cout<<x<<" ";
+  return 0;
+}
